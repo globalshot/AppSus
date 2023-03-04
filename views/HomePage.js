2a76d5a0caca4edba6c11c2381d9c9923fdd74b1
steven <stevenbuch14@gmail.com>
2023-03-04 20:32:57 +0200

    fast home styling

--- views/HomePage.js
+++ views/HomePage.js
@@ -1,7 +1,29 @@
+
 export default {
 	template: `
+    <div class="home-background-black">
         <section class="home-page">
             <h1>Home sweet home</h1>
         </section>
+        <div class="follower" :style="{top: mouseY + 'px', left: mouseX + 'px'}"></div>
+        </div>
     `,
+        data() {
+          return {
+            mouseX: 0,
+            mouseY: 0
+          }
+        },
+        mounted() {
+          document.addEventListener('mousemove', this.handleMouseMove);
+        },
+        beforeDestroy() {
+          document.removeEventListener('mousemove', this.handleMouseMove);
+        },
+        methods: {
+          handleMouseMove(event) {
+            this.mouseX = event.clientX;
+            this.mouseY = event.clientY;
+          }
+        }
 }
