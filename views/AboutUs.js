42a11ecc4bf85bf9c91ccc1a3e815f3ceed4040f
steven <stevenbuch14@gmail.com>
2023-03-04 21:06:18 +0200

    text changed some

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -5,7 +5,7 @@ export default {
             <pre>
         we are kamal and steven, 
         both learning full stacks, this is our 3rd spring,
-        the ending was the most challenging, fast css as much as possible,
+        the ending was the most challenging, making things looks good at the end,
         had fun practicing and doing it fast, while other things were slower</pre>
         </section>
     `
