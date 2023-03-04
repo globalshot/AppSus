1751e23e707cf7a684fb0350fc340423694de79c
steven <stevenbuch14@gmail.com>
2023-03-04 20:59:24 +0200

    last styling

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -2,6 +2,11 @@ export default {
 	template: `
         <section class="about-page">
             <h1>About us</h1>
+            <pre>
+        we are kamal and steven, 
+        both learning full stacks, this is our 3rd spring,
+        the ending was the most challenging, fast css as much as possible,
+        had fun practicing and doing it fast, while others were slower</pre>
         </section>
     `
 }
